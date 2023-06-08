29cae67edd532a70519f9ba5b52a406e7ba18906
scarter86 <samuelcarter1@gmail.com>
2023-06-08 14:49:30 +0100

    <added age verification>

--- apps.js
+++ apps.js
@@ -1,2 +1,13 @@
-String name = "Sam";
-System.out.println(name);
+let ageStr = prompt('How old are you?');
+let age = Number(ageStr);
+
+let feedback = age >= 18 ?
+   'You are in!':
+   'You must be 18 years or older to enter';
+
+
+alert(feedback);
+
+if (person != null) {
+  document.getElementById("demo").innerHTML =
+  "Hello " + person + "! How are you today?";}
