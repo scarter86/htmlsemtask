6e68c5a9891910de9d84c2807c38a4998e6c6e8c
scarter86 <samuelcarter1@gmail.com>
2023-06-09 16:00:42 +0100

    update age page links

--- apps.js
+++ apps.js
@@ -2,15 +2,6 @@ let Username = prompt ("Nice to meet you, what's your name?");
 console.log (Username)
 const Welcomemsg = alert("Welcome to Pop up Prompt Paradise " + Username);
 
-let ageStr = prompt('How old are you?');
-let age = Number(ageStr);
-
-let feedback = age >= 18 ?
-   'You are in!':
-   'You must be 18 years or older to enter';
-
-alert(feedback);
-
 alert('Welcome!')
 
 function myFunction() {
